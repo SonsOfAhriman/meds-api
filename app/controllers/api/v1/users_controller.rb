702cdcd1e883024cced73122a519fa1a523d5e17
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-12-20 20:22:45 +0900

    added user index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
+    def index
+        @users = policy_scope(User)
+    end
+
     def show
         @user = User.find(params[:id])
         authorize @user  # For Pundit
