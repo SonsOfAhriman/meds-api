8b6720e09e452df8b1d24338b808ec5315f930a2
Ryan Huddleston <wryanhuddleston@gmail.com>
2022-01-06 13:46:43 +0900

    added auth user to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ActionController::RequestForgeryProtection
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   # skip_before_action :verify_authenticity_token, :only => :create
 
